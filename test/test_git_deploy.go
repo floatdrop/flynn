14e73f9d10328b24e1de5ddc457b0278e5995114
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-02-20 14:04:33 +0900

    test: Confirm that blobstore caching works.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- test/test_git_deploy.go
+++ test/test_git_deploy.go
@@ -57,6 +57,27 @@ func (s *GitDeploySuite) TestBuildCaching(t *c.C) {
 	t.Assert(push, OutputContains, "cached: 1")
 }
 
+func (s *GitDeploySuite) TestRepoCaching(t *c.C) {
+	r := s.newGitRepo(t, "empty")
+	t.Assert(r.flynn("create"), Succeeds)
+
+	r.git("commit", "-m", "bump", "--allow-empty")
+	r.git("commit", "-m", "bump", "--allow-empty")
+	push := r.git("push", "flynn", "master")
+	t.Assert(push, Succeeds)
+	t.Assert(push, c.Not(OutputContains), "cached")
+
+	// cycle the receiver to clear any cache
+	t.Assert(flynn(t, "/", "-a", "gitreceive", "scale", "app=0"), Succeeds)
+	t.Assert(flynn(t, "/", "-a", "gitreceive", "scale", "app=1"), Succeeds)
+
+	r.git("commit", "-m", "bump", "--allow-empty")
+	push = r.git("push", "flynn", "master")
+	t.Assert(push, Succeeds)
+	// should only contain one object
+	t.Assert(push, OutputContains, "Counting objects: 1, done.")
+}
+
 func (s *GitDeploySuite) TestGoBuildpack(t *c.C) {
 	s.runBuildpackTest(t, "go-flynn-example", []string{"postgres"})
 }
